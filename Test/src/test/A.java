0edc3b3baf4e6a9e615e4e6f55bddde000241897
USER <USER@DESKTOP-TFM8BVD>
2025-01-09 18:34:12 +0900

    aaa

--- Test/src/test/A.java
+++ Test/src/test/A.java
@@ -3,19 +3,5 @@ package test;
 public class A {
 	public void printA() {
 		System.out.println("A");
-		System.out.println("A1");
-		System.out.println("A2");
-		System.out.println("A3");
-		System.out.println("A4");
-		System.out.println("A5");
-		System.out.println("A6");
-		System.out.println("A7");
-		System.out.println("A10");
-		System.out.println("A8");
-		System.out.println("A9");
-		System.out.println("A11");
-		System.out.println("hello");
-		System.out.println("A12");
-
 	}
 }
