e20e2bde10725785863ab60cd183f6c437395e6d
USER <USER@DESKTOP-TFM8BVD>
2025-01-09 18:34:36 +0900

    aabbcc

--- Test/src/test/B.java
+++ Test/src/test/B.java
@@ -3,7 +3,5 @@ package test;
 public class B {
 	public void printA() {
 		System.out.println("B");
-		System.out.println("B1");
-		System.out.println("B2");
 	}
 }
--- Test/src/test/C.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class C {
-	public void printA() {
-		System.out.println("C");
-		System.out.println("C1");
-		System.out.println("C2");
-	}
-}
